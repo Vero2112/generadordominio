7550e1b033cbecf5c152b61f4adc73121362141f
Vero2112 <verorodriguezruiz21@gmail.com>
2022-05-02 18:38:33 +0000

    nuevo ejercicio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dominio = ["com", ".net", ".us", ".io"];
+
+  let sumadestrings = "";
+  let pronombrealeatorio = Math.floor(Math.random() * 2);
+  let adjaleatorio = Math.floor(Math.random() * 2);
+  let nounaleatorio = Math.floor(Math.random() * 2);
+  let dominioaleatorio = Math.floor(Math.random() * 2);
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let r = 0; r < adj.length; r++) {
+      for (let v = 0; v < noun.length; v++) {
+        for (let d = 0; d < dominio.length; d++) {}
+      }
+    }
+  }
+
+  sumadestrings +=
+    pronoun[pronombrealeatorio] +
+    adj[adjaleatorio] +
+    noun[nounaleatorio] +
+    dominio[dominioaleatorio];
+
+  console.log("www." + sumadestrings);
   console.log("Hello Rigo from the console!");
 };
